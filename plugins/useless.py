5b5eac7bd3e6d466dded9094297075e77c6356e1
Trinity Mods <trinitymods.ofc@gmail.com>
2025-07-04 00:22:20 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,1 +1,42 @@
+# ────────────────────────────────────────────────────────────────
 
+# ✅ THIS PROJECT IS DEVELOPED AND MAINTAINED BY @trinityXmods (TELEGRAM)
+# 🚫 DO NOT REMOVE OR ALTER THIS CREDIT LINE UNDER ANY CIRCUMSTANCES.
+
+# ⭐ FOR MORE HIGH-QUALITY OPEN-SOURCE BOTS, FOLLOW US ON GITHUB.
+# 🔗 OFFICIAL GITHUB: https://github.com/Trinity-Mods
+# 📩 NEED HELP OR HAVE QUESTIONS? REACH OUT VIA TELEGRAM: @velvetexams
+
+# ────────────────────────────────────────────────────────────────
+
+from bot import Bot
+from pyrogram.types import Message
+from pyrogram import filters
+from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
+from datetime import datetime
+from helper_func import get_readable_time
+
+@Bot.on_message(filters.command('stats') & filters.user(ADMINS))
+async def stats(bot: Bot, message: Message):
+    now = datetime.now()
+    delta = now - bot.uptime
+    time = get_readable_time(delta.seconds)
+    await message.reply(BOT_STATS_TEXT.format(uptime=time))
+
+
+@Bot.on_message(filters.private & ~filters.user(ADMINS) & ~filters.command(['start','ping', 'ch2l', 'cancel']))
+# @Bot.on_message(filters.private & filters.incoming)
+async def useless(_, message: Message):
+    if USER_REPLY_TEXT:
+        await message.reply(USER_REPLY_TEXT)
+
+# ────────────────────────────────────────────────────────────────
+
+# ✅ THIS PROJECT IS DEVELOPED AND MAINTAINED BY @trinityXmods (TELEGRAM)
+# 🚫 DO NOT REMOVE OR ALTER THIS CREDIT LINE UNDER ANY CIRCUMSTANCES.
+
+# ⭐ FOR MORE HIGH-QUALITY OPEN-SOURCE BOTS, FOLLOW US ON GITHUB.
+# 🔗 OFFICIAL GITHUB: https://github.com/Trinity-Mods
+# 📩 NEED HELP OR HAVE QUESTIONS? REACH OUT VIA TELEGRAM: @velvetexams
+
+# ────────────────────────────────────────────────────────────────  
